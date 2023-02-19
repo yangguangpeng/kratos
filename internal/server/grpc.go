0aae501df6a1f74048173e12db72cd88f30afe32
yangguangpeng <646138471@qq.com>
2023-02-19 20:57:27 +0800

    自定义中间件，打印来自客户端metadata信息

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -4,12 +4,10 @@ import (
 	"context"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware"
-	"github.com/go-kratos/kratos/v2/middleware/auth/jwt"
 	"github.com/go-kratos/kratos/v2/middleware/logging"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
-	jwtv4 "github.com/golang-jwt/jwt/v4"
 	v1 "helloworld/api/helloworld/v1"
 	"helloworld/internal/conf"
 	"helloworld/internal/service"
@@ -17,14 +15,14 @@ import (
 
 // NewGRPCServer new a gRPC server.
 func NewGRPCServer(c *conf.Server, greeter *service.GreeterService, demo *service.DemoService, logger log.Logger) *grpc.Server {
-	testKey := "testKey"
 	var opts = []grpc.ServerOption{
 		grpc.Middleware(
 			recovery.Recovery(),
 			logging.Server(logger),
-			jwt.Server(func(token *jwtv4.Token) (interface{}, error) {
-				return []byte(testKey), nil
-			}),
+			//jwt验证中间件
+			//jwt.Server(func(token *jwtv4.Token) (interface{}, error) {
+			//	return []byte("testKey"), nil
+			//}),
 			MyMiddleware(),
 		),
 	}
