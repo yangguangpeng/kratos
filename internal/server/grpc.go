395c712243c250fe0b999ee56cd4979896056202
yangguangpeng <646138471@qq.com>
2023-02-19 11:59:43 +0800

    通过中间件输入到请求日志文件

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -1,20 +1,22 @@
 package server
 
 import (
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
 	v1 "helloworld/api/helloworld/v1"
 	"helloworld/internal/conf"
 	"helloworld/internal/service"
-
-	"github.com/go-kratos/kratos/v2/log"
-	"github.com/go-kratos/kratos/v2/middleware/recovery"
-	"github.com/go-kratos/kratos/v2/transport/grpc"
 )
 
 // NewGRPCServer new a gRPC server.
-func NewGRPCServer(c *conf.Server, greeter *service.GreeterService, demo *service.DemoService,logger log.Logger) *grpc.Server {
+func NewGRPCServer(c *conf.Server, greeter *service.GreeterService, demo *service.DemoService, logger log.Logger) *grpc.Server {
+
 	var opts = []grpc.ServerOption{
 		grpc.Middleware(
 			recovery.Recovery(),
+			logging.Server(logger),
 		),
 	}
 	if c.Grpc.Network != "" {
