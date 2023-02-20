2dadca10f3e23b939674c09189b89c0fc6a044c6
yangguangpeng <646138471@qq.com>
2023-02-20 23:11:10 +0800

    服务注册与发现

--- cmd/helloworld/wire_gen.go
+++ cmd/helloworld/wire_gen.go
@@ -7,6 +7,7 @@
 package main
 
 import (
+	"github.com/go-kratos/kratos/contrib/registry/consul/v2"
 	"github.com/go-kratos/kratos/v2"
 	"github.com/go-kratos/kratos/v2/log"
 	"helloworld/internal/biz"
@@ -23,7 +24,7 @@ import (
 // Injectors from wire.go:
 
 // wireApp init kratos application.
-func wireApp(confServer *conf.Server, confData *conf.Data, logger log.Logger) (*kratos.App, func(), error) {
+func wireApp(confServer *conf.Server, confData *conf.Data, logger log.Logger, registry *consul.Registry) (*kratos.App, func(), error) {
 	dataData, cleanup, err := data.NewData(confData, logger)
 	if err != nil {
 		return nil, nil, err
@@ -36,7 +37,7 @@ func wireApp(confServer *conf.Server, confData *conf.Data, logger log.Logger) (*
 	demoService := service.NewDemoService(demoUsecase)
 	grpcServer := server.NewGRPCServer(confServer, greeterService, demoService, logger)
 	httpServer := server.NewHTTPServer(confServer, greeterService, demoService, logger)
-	app := newApp(logger, grpcServer, httpServer)
+	app := newApp(logger, grpcServer, httpServer, registry)
 	return app, func() {
 		cleanup()
 	}, nil
